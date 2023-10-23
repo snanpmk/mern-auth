c12067eb3b5d916dfc74139ca400d754c4ecdcd4
Sinan <pmksinan@gmail.com>
2023-10-23 14:21:52 +0530

    create mongoose schema

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config()
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("Connected to MongoDb Successfully 🚀");
-}).catch((error) => {
-    console.log(error);
-})
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to MongoDb Successfully 🚀");
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 const app = express()
 app.listen(3000, () => {
     console.log('server listening on port 3000!');
