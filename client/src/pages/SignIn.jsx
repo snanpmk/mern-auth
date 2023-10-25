31d108ec3ba12e2ec9bbbc4b3f4c1e2db18726ba
Sinan <pmksinan@gmail.com>
2023-10-25 11:55:00 +0530

    complete the signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function SignIn() {
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleOnChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      setLoading(true);
+      setError(false);
+      e.preventDefault();
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success == false) {
+        setError(true);
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-extrabold uppercase my-7">
+        Sign In
+      </h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          autoComplete="Email"
+          onChange={handleOnChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          autoComplete="password"
+          onChange={handleOnChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading...." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign Up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 font-semibold mt-5">
+        {error && "something went wrong!!"}
+      </p>
+    </div>
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
 
   const handleOnChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -28,6 +29,7 @@ export default function SignUp() {
         setError(true)
       }
       setLoading(false);
+      navigate("/")
     } catch (error) {
       setLoading(false);
       setError(true);
