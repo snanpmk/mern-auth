ee3e86cf6be76fe4aacc7f0fa333bec35f2e4d90
Sinan <pmksinan@gmail.com>
2023-10-30 13:46:06 +0530

    protected profile route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -7,8 +7,12 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const currentUser = useSelector((state) => state.user);
+  console.log(currentUser, "////");
+
   return (
     <BrowserRouter>
       <Header />
@@ -17,9 +21,15 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/profile" element={<Profile />} />
-        </Route>
+        <Route
+          exact path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        
       </Routes>
     </BrowserRouter>
   );
--- client/src/redux/users/userSlice.js
+++ client/src/redux/users/userSlice.js
@@ -25,5 +25,5 @@ const userSlice = createSlice({
     }
 })
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
 export default userSlice.reducer;
